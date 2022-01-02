1b6744f300965f68c0a48a4538110be51a9b6544
Oscar Laird <olaird25@gmail.com>
2022-01-01 19:07:14 -0700

    remove my accidental inclusion of the build and dist directories. Add these to .gitignore

--- build/lib/vinca/__init__.py
+++
@@ -1,5 +0,0 @@
-from vinca import _cli_objects
-for name, obj in vars(_cli_objects).items():
-	if name.startswith('_'):
-		continue
-	globals()[name] = obj
--- build/lib/vinca/_generators/__init__.py
+++
@@ -1,5 +0,0 @@
-from vinca._generators.two_lines import two_lines
-from vinca._generators.verses import verses
-
-generators_dict = {'2': two_lines,
-		   'v': verses}
