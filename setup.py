5b8ab0b18d9aee1ef108496315884d895d9b6527
Oscar Laird <olaird25@gmail.com>
2022-01-02 00:38:48 -0700

    Improve setup.py to show more information to pip

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 import setuptools
+from pathlib import Path
 
 setuptools.setup(
+	version="134",
 	name="vinca",
-	version="131",
 	author="Oscar Laird", 
+	autor_email = "olaird25@gmail.com",
+
+	description = "simple spaced repetition",
+	long_description = (Path(__file__).parent / 'README.md').read_text(),
+	long_description_content_type="text/markdown",
+
+	url = 'https://github.com/oscarlaird/vinca-SRS',
+	project_urls={
+		"Bug Tracker": "https://github.com/oscarlaird/vinca-SRS/issues",
+		"Online Man Page": "https://oscarlaird.github.io/vinca-SRS/vinca.1.html",
+	},
+
 	data_files = [('man/man1', ['docs/vinca.1'])],
 	include_package_data = True,
 	install_requires = ['fire'],
